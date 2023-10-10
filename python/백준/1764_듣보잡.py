7844bc679e8041336706b2dffe075b5f3cc10195
SangNyungLee <lsy6747@gmail.com>
2023-10-11 03:40:41 +0900

    백준

--- python/백준/1764_듣보잡.py
+++ python/백준/1764_듣보잡.py
@@ -1,19 +1,36 @@
-import sys
-n, m = map(int, sys.stdin.readline().rstrip().split())
-dic={}
-arr=[]
-result = 0
-for i in range(n):
-    a = str(sys.stdin.readline().rstrip())
-    dic[a] = 0
-for i in range(m):
-    a = str(sys.stdin.readline().rstrip())
-    if a in dic:
-        result += 1
-        arr.append(a)
-arr.sort()
-print(result)
-for i in arr:
-    print(i)
-
-
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import sys\n",
+    "n, m = map(int, sys.stdin.readline().rstrip().split())\n",
+    "dic={}\n",
+    "arr=[]\n",
+    "result = 0\n",
+    "for i in range(n):\n",
+    "    a = str(sys.stdin.readline().rstrip())\n",
+    "    dic[a] = 0\n",
+    "for i in range(m):\n",
+    "    a = str(sys.stdin.readline().rstrip())\n",
+    "    if a in dic:\n",
+    "        result += 1\n",
+    "        arr.append(a)\n",
+    "arr.sort()\n",
+    "print(result)\n",
+    "for i in arr:\n",
+    "    print(i)"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
