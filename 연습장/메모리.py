6314c091d0fa68553a9a962aceed0a2d3e8e2b20
SangNyungLee <lsy6747@gmail.com>
2023-11-03 19:10:58 +0900

    백준

---
+++ 연습장/메모리.py
@@ -0,0 +1,9 @@
+import sys
+
+# print(sys.getsizeof(2**29))
+# print(sys.getsizeof(2**30))
+# print(sys.getsizeof(2**31))
+# print(sys.getsizeof("1"))
+# print(sys.getsizeof("1234567890"))
+print(sys.getsizeof("1"))
+print(sys.getsizeof("안녕"))
