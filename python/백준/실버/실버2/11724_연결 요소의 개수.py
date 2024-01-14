ed0389a5206edf314f26bf37683f8183ea0caf89
SangNyungLee <lsy6747@gmail.com>
2024-01-14 22:10:21 +0900

    백준

--- python/백준/실버/실버2/11724_연결 요소의 개수.py
+++ python/백준/실버/실버2/11724_연결 요소의 개수.py
@@ -33,3 +33,32 @@ for i in range(1, N + 1):
         visited[i] = True
         bfs()
 print(result)
+
+#### DFS 풀이 ####
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10000)
+N, M = map(int, input().split())
+graph = [[False] * (N + 1) for _ in range(N + 1)]
+visited = [False] * (N + 1)
+result = 0
+
+
+def dfs(idx):
+    global result
+    visited[idx] = True
+    for i in range(N + 1):
+        if not visited[i] and graph[idx][i]:
+            dfs(i)
+
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a][b] = True
+    graph[b][a] = True
+
+for i in range(1, N + 1):
+    if visited[i] == False:
+        dfs(i)
+        result += 1
+print(result)
