21dce8b88ed09ae166b12444027028f11e9e7d35
SangNyungLee <lsy6747@gmail.com>
2024-01-15 17:37:54 +0900

    백준

--- 연습장/트리연습장.py
+++ 연습장/트리연습장.py
@@ -3,53 +3,6 @@ from collections import deque
 
 input = sys.stdin.readline
 
-N = int(input())
-graph = []
-q = []
-
-for i in range(N):
-    graph.append(list(map(int, input().split())))
-maxNum = max(map(max, graph))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
-def bfs(x, y, graph2):
-    queue = deque()
-    queue.append((x, y))
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= N or ny >= N:
-                continue
-            if graph2[nx][ny] != 0:
-                graph2[nx][ny] = 0
-                queue.append((nx, ny))
-    return 1
-
-
-for k in range(0, maxNum + 1):
-    result = 0
-    for p in graph:
-        graph2 = [[0 if element <= k else element for element in row] for row in graph]
-    for i in range(N):
-        for j in range(N):
-            if graph2[i][j] != 0 and graph2[i][j] > k:
-                result += bfs(i, j, graph2)
-    q.append(result)
-print(max(q))
-
-
-###############################
-
-import sys
-from collections import deque
-
-input = sys.stdin.readline
-
 
 # 3. bfs함수 구현
 def bfs(x, y, high):
