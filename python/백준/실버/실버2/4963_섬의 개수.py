24bc416284794efedfcffd750686256854c4a3a2
SangNyungLee <lsy6747@gmail.com>
2024-01-17 02:54:57 +0900

    백준

---
+++ python/백준/실버/실버2/4963_섬의 개수.py
@@ -0,0 +1,90 @@
+############### BFS #################
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+# 대각선도 움직임이 가능하다.
+dx = [-1,-1,-1,1, 1,1, 0, 0]
+dy = [0, 1, -1,-1, 1,0, -1, 1]
+
+island = 0 # 섬 개수 출력
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x,y = queue.popleft()
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= h or ny >= w or graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append((nx,ny))
+    return 1
+
+while True:
+    w,h = map(int, input().split())
+    island = 0 # 섬 개수 출력
+    # w,h가 0이 되면 반복문 종료
+    if w == 0 and h == 0:
+        break
+    graph = []
+    
+    # 좌표 추가
+    for i in range(h):
+        graph.append(list(map(int, input().split())))
+
+    # 그래프 하나씩 탐색하기
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == 1:
+                island += bfs(i,j)
+    print(island)
+
+
+########### DFS #############
+import sys
+sys.setrecursionlimit(2500)
+input = sys.stdin.readline
+
+# 대각선도 움직임이 가능하다.
+dx = [-1,-1,-1,1, 1,1, 0, 0]
+dy = [0, 1, -1,-1, 1,0, -1, 1]
+
+
+def dfs(x,y):
+    if x < 0 or y < 0 or x >= h or y >= w or graph[x][y] == 0:
+        return False
+    
+    visited[x][y] = True
+    graph[x][y] = 0
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        dfs(nx,ny)
+    return True
+
+while True:
+    w,h = map(int, input().split())
+    island = 0 # 섬 개수 출력
+    # w,h가 0이 되면 반복문 종료
+    if w == 0 and h == 0:
+        break
+    graph = []
+    
+    # 좌표 추가
+    for i in range(h):
+        graph.append(list(map(int, input().split())))
+
+    # 방문한 곳 표시
+    visited = [[False] * (w) for _ in range(h)]
+    result = 0
+    # 그래프 하나씩 탐색하기
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == 1 and visited[i][j] == False:
+                dfs(i,j)
+                result += 1
+    print(result)
--- 연습장/트리연습장.py
+++ 연습장/트리연습장.py
@@ -1,45 +1,43 @@
 import sys
-from collections import deque
-
+sys.setrecursionlimit(2500)
 input = sys.stdin.readline
 
-
-# 3. bfs함수 구현
-def bfs(x, y, high):
-    queue = deque()
-    queue.append((x, y))
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                continue
-            if graph[nx][ny] > high and visited[nx][ny] == 0:
-                visited[nx][ny] = 1
-                queue.append((nx, ny))
+# 대각선도 움직임이 가능하다.
+dx = [-1,-1,-1,1, 1,1, 0, 0]
+dy = [0, 1, -1,-1, 1,0, -1, 1]
 
 
-# 1. 입력값 받기
-n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-# 최대 높이값 구하기
-high = max(map(max, graph))
-# 방향
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+def dfs(x,y):
+    if x < 0 or y < 0 or x >= h or y >= w or graph[x][y] == 0:
+        return False
+    
+    visited[x][y] = True
+    graph[x][y] = 0
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        dfs(nx,ny)
+    return True
 
-MAX = 0
-# 2. 수심에 따라 달라지는 안전영역 갯수 비교
-for t in range(high):
-    visited = [[0] * n for _ in range(n)]
-    cnt = 0
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] > t and visited[i][j] == 0:
-                visited[i][j] = 1  # 방문표시
-                bfs(i, j, t)
-                cnt += 1
-    if MAX < cnt:
-        MAX = cnt
-print(MAX)
+while True:
+    w,h = map(int, input().split())
+    island = 0 # 섬 개수 출력
+    # w,h가 0이 되면 반복문 종료
+    if w == 0 and h == 0:
+        break
+    graph = []
+    
+    # 좌표 추가
+    for i in range(h):
+        graph.append(list(map(int, input().split())))
+
+    # 방문한 곳 표시
+    visited = [[False] * (w) for _ in range(h)]
+    result = 0
+    # 그래프 하나씩 탐색하기
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == 1 and visited[i][j] == False:
+                dfs(i,j)
+                result += 1
+    print(result)
