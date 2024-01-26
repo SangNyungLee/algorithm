f4ed85376e3e59b0ad3aca060361e750d7e4dd0f
SangNyungLee <lsy6747@gmail.com>
2024-01-26 20:25:43 +0900

    백준

--- python/백준/실버/실버4/2217_로프.py
+++ python/백준/실버/실버4/2217_로프.py
@@ -9,13 +9,9 @@ for _ in range(n):
     arr.append(int(input()))
 
 arr.sort(reverse=True)
-print(arr)
-print(arr[0])
 MAX = 0  # 최대 중량 재는거
 for i in range(n, 0, -1):
-    print("i", i)
     w = i * arr[i - 1]
-    print("w", w)
     if w > MAX:
         arr.pop()
         MAX = w
