9ece63c718fddf3ee05f6f9558c557bc7db6798b
SangNyungLee <lsy6747@gmail.com>
2024-02-14 01:41:26 +0900

    백준

--- 연습장/트리연습장.py
+++ 연습장/트리연습장.py
@@ -1,23 +1,47 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
+n, m = map(int, input().split())
+INF = int(1e9)
+check = [[INF] * (m + 1) for _ in range(n + 1)]
+graph = [[]]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+result = 0
 
-n, m, v = map(int, input().split())
-graph = [[False] * (n + 1) for _ in range(n + 1)]
-visited = [False] * (n + 1)
-
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = True
-    graph[b][a] = True
-
-
-def dfs(idx):
-    visited[idx] = True
-    print(idx, end=" ")
-    for i in range(1, n + 1):
-        if not visited[i] and graph[idx][i]:
-            dfs(i)
+for _ in range(n):
+    a = str(input().rstrip())
+    graph.append(list(a))
 
 
-dfs(v)
+def bfs(sx, sy):
+    global result
+    queue = deque()
+    queue.append((sx, sy))
+    check[sx][sy] = True
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx <= 0 or ny <= 0 or nx >= n or ny >= m:
+                continue
+            if graph[nx][ny] == 0 and check[nx][ny] != INF:
+                check[nx][ny] = result
+                queue.append((nx, ny))
+    if nx == n and ny == m:
+        print(result)
+    else:
+        result = result + 1
+        for i in range(4):
+            nnx = nx + dx[i]
+            nny = ny + dy[i]
+            if nnx <= 0 or nny <= 0 or nnx >= n or nny >= m:
+                continue
+            graph[nnx][nny] = 0
+        bfs(nx, ny)
+
+
+bfs(1, 1)
