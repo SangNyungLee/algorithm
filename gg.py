a84cf0516c710cdec72720a6605e9ba966584127
SangNyungLee <lsy6747@gmail.com>
2024-02-25 15:14:22 +0900

    알고리즘

--- gg.py
+++ gg.py
@@ -3,14 +3,14 @@ import heapq
 
 input = sys.stdin.readline
 
-INF = int(1e9)
 V, E = map(int, input().split())
 K = int(input().rstrip())
 graph = [[] for _ in range(V + 1)]
-distance = [INF] * (V + 1)
 for _ in range(E):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
+INF = int(1e9)
+distance = [INF] * (V + 1)
 
 
 def djikstra(start):
@@ -22,7 +22,6 @@ def djikstra(start):
         if distance[now] < dist:
             continue
         for i in graph[now]:
-            # i에는 i[0] : 내가 가는 곳이랑 i[1] : 거기까지의 거리가 적혀있음
             cost = dist + i[1]
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
@@ -30,6 +29,7 @@ def djikstra(start):
 
 
 djikstra(K)
+
 for i in range(1, V + 1):
     if distance[i] == INF:
         print("INF")
