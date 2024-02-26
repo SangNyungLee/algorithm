d57dc174b1c947a754d5809136e63a2c5fdee535
SangNyungLee <lsy6747@gmail.com>
2024-02-27 00:40:11 +0900

    알고리즘

--- 연습장/트리 순회.py
+++ 연습장/트리 순회.py
@@ -5,32 +5,36 @@ class Node:
         self.right_node = right_node
 
 
+# 전위 순회(Preorder Traversal)
 def pre_order(node):
-    print(node.data, end="")
-    if node.left_node:
+    print(node.data, end=" ")
+    if node.left_node != None:
         pre_order(tree[node.left_node])
-    if node.right_node:
+    if node.right_node != None:
         pre_order(tree[node.right_node])
 
 
+# 중위 순회(Inorder Traversal)
 def in_order(node):
-    if node.left_node:
+    if node.left_node != None:
         in_order(tree[node.left_node])
-    print(node.data, end="")
-    if node.right_node:
+    print(node.data, end=" ")
+    if node.right_node != None:
         in_order(tree[node.right_node])
 
 
+# 후위 순회(Postorder Traversal)
 def post_order(node):
-    if node.left_node:
+    if node.left_node != None:
         post_order(tree[node.left_node])
-    if node.right_node:
+    if node.right_node != None:
         post_order(tree[node.right_node])
-    print(node.data, end="")
+    print(node.data, end=" ")
 
 
 n = int(input())
 tree = {}
+
 for i in range(n):
     data, left_node, right_node = input().split()
     if left_node == ".":
