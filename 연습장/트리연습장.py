f0a5f746f734c9063e0ec0675fb695591ead9543
SangNyungLee <lsy6747@gmail.com>
2024-02-29 00:08:31 +0900

    알고리즘

--- 연습장/트리연습장.py
+++ 연습장/트리연습장.py
@@ -1,46 +1,12 @@
 class Node:
-    def __init__(self, data, left_node, right_node):
+    def __init__(self, data, node_left, node_right):
         self.data = data
-        self.left_node = left_node
-        self.right_node = right_node
-
-
+        self.node_left = node_left
+        self.node_right = node_right
 def pre_order(node):
-    print(node.data, end="")
-    if node.left_node:
-        pre_order(tree[node.left_node])
-    if node.right_node:
-        pre_order(tree[node.right_node])
-
-
-def in_order(node):
-    if node.left_node:
-        in_order(tree[node.left_node])
-    print(node.data, end="")
-    if node.right_node:
-        in_order(tree[node.right_node])
-
-
-def post_order(node):
-    if node.left_node:
-        post_order(tree[node.left_node])
-    if node.right_node:
-        post_order(tree[node.right_node])
-    print(node.data, end="")
-
 
 n = int(input())
-tree = {}
-for i in range(n):
-    data, left_node, right_node = input().split()
-    if left_node == ".":
-        left_node = None
-    if right_node == ".":
-        right_node = None
-    tree[data] = Node(data, left_node, right_node)
-
-pre_order(tree["A"])
-print()
-in_order(tree["A"])
-print()
-post_order(tree["A"])
+tree ={}
+for _ in range(n):
+    data, node_left, node_right = input().split()
+    tree[data] = Node(data, node_left, node_right)
