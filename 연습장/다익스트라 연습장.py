2883f4192a4593e9786a03495b2a1f1f1a3525d5
SangNyungLee <lsy6747@gmail.com>
2024-05-23 16:56:23 +0900

    알고리즘연습

--- 연습장/다익스트라 연습장.py
+++ 연습장/다익스트라 연습장.py
@@ -1,21 +1,15 @@
-import sys
-import heapq
-
+import sys, heapq
 input = sys.stdin.readline
-
-n, e = map(int, input().split())
 INF = int(1e9)
-graph = [[] for _ in range(n + 1)]
-
+v,e = map(int, input().split())
+start = int(input().rstrip())
+graph = [[]for _ in range(v + 1)]
+distance = [INF] * (v + 1)
 for _ in range(e):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-    graph[b].append((a, c))
-v1, v2 = map(int, input().split())
+    a,b,c = map(int, input().split())
+    graph[a].append((b,c))
 
-
-def djikstra(start):
-    distance = [INF] * (n + 1)
+def dijkstra(start):
     queue = []
     heapq.heappush(queue, (0, start))
     distance[start] = 0
@@ -28,18 +22,9 @@ def djikstra(start):
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(queue, (cost, i[0]))
-    return distance
-
-
-origin_distance = djikstra(1)
-v1_distance = djikstra(v1)
-v2_distance = djikstra(v2)
-
-v1_path = origin_distance[v1] + v1_distance[v2] + v2_distance[n]
-v2_path = origin_distance[v2] + v2_distance[v1] + v1_distance[n]
-
-result = min(v1_path, v2_path)
-if v1_path >= INF or v2_path >= INF:
-    print(-1)
-else:
-    print(result)
+dijkstra(start)
+for i in range(1, v + 1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
--- 연습장/트리연습장.py
+++ 연습장/트리연습장.py
@@ -1,6 +1,34 @@
-# 2진 변환
-s = 5
-nn = format(s, "b")
-mm = bin(s)[2:]
-print(nn)
-print(mm)
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n,m,v = map(int , input().split())
+graph = [[0] * (n + 1)for _ in range(n + 1)]
+visited = [False] * (n + 1)
+for _ in range(m):
+    a,b = map(int, input().split())
+    graph[a][b] = True
+    graph[b][a] = True
+
+def bfs(start):
+    queue = deque()
+    queue.append(start)
+    visited[start] = True
+    while queue:
+        cur = queue.popleft()
+        print(cur, end=" ")
+        for i in range(1, n + 1):
+            if not visited[i] and graph[cur][i]:
+                queue.append(i)
+                visited[i] = True
+def dfs(start):
+    visited[start] = True
+    print(start, end=" ")
+    for i in range(1, n + 1):
+        if not visited[i] and graph[start][i]:
+            dfs(i)
+
+dfs(v)
+print()
+visited = [False] * (n + 1)
+bfs(v)
