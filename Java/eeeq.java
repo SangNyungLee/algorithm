c4ee558c08e6f6279d63b92554d1804e34785d6e
SangNyungLee <lsy6747@gmail.com>
2024-12-30 17:08:59 +0900

    add java_main

--- Java/eeeq.java
+++ Java/eeeq.java
@@ -1,4 +1,27 @@
+import java.util.*;
+
 public class eeeq {
     public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        Deque<Integer> deque = new LinkedList<>();
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+
+        deque.add(1);
+        deque.add(2);
+        deque.add(3);
+
+        System.out.println("queue = " + queue.remove());
+
+        System.out.println("deque = " + deque.remove());
+
+        System.out.println("stack = " + stack.pop());
     }
 }
--- gg.py
+++ gg.py
@@ -1,16 +1,13 @@
 from collections import deque
 import sys
 
-input = sys.stdin.readline
 
-n, m, v = map(int, input().split())
-graph = [[False] * (n + 1) for _ in range(n + 1)]
-visited = [False] * (n + 1)
-
-for i in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = True
-    graph[b][a] = True
+def dfs(idx):
+    visited[idx] = True
+    print(idx, end=" ")
+    for i in range(1, n + 1):
+        if not visited[i] and graph[idx][i]:
+            dfs(i)
 
 
 def bfs(start):
@@ -21,21 +18,20 @@ def bfs(start):
         cur = queue.popleft()
         print(cur, end=" ")
         for i in range(1, n + 1):
-            if not visited[i] and graph[i][cur]:
+            if not visited[i] and graph[cur][i]:
                 queue.append(i)
                 visited[i] = True
 
 
-def dfs(start):
-    visited[start] = True
-    print(start, end=" ")
-    for i in range(1, n + 1):
-        if not visited[i] and graph[i][start]:
-            dfs(i)
-
+n, m, v = list(map(int, input().split()))
+graph = [[False] * (n + 1) for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = True
+    graph[b][a] = True
+visited = [False] * (n + 1)
 
 dfs(v)
-
-visited = [False] * (n + 1)
 print()
+visited = [False] * (n + 1)
 bfs(v)
