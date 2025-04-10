4c8d85bdd1c4c632e036f979425cafc910ad66fd
sanglee <lsy6747@gmail.com>
2025-04-10 20:45:07 +0900

    알고리즘 연습

--- python/gg.py
+++ python/gg.py
@@ -1,10 +1,11 @@
-import heapq, sys
-input = sys.stdin.readline
+import heapq
+
 V, E = map(int, input().split())
 start = int(input())
-graph = [[] for _ in range(V + 1)]
 INF = int(1e9)
 distance = [INF] * (V + 1)
+graph = [[] for _ in range(V + 1)]
+
 for _ in range(E):
     a,b,c = map(int, input().split())
     graph[a].append((b,c))
@@ -24,9 +25,4 @@ def dijkstra(start):
                 heapq.heappush(queue, (cost, next_node))
 
 dijkstra(start)
-
-for i in range(1, V + 1):
-    if distance[i] == INF:
-        print("INF")
-    else:
-        print(distance[i])
+print(distance)
