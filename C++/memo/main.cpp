3371ef043ed19981c5be1b76fcc59b13237c16ae
sanglee <lsy6747@gmail.com>
2025-05-26 23:30:11 +0900

    modify directory

--- C++/memo/main.cpp
+++ C++/memo/main.cpp
@@ -1,26 +1,49 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(void){
+// 	ios::sync_with_stdio(0);
+// 	cin.tie(0);
+// 	stack<pair<char, int>> st;
+// 	string str;
+// 	cin >> str;
+// 	for(char c : str){
+// 		// 여는 괄호일 경우에는 그냥 stack에 push해줌
+// 		if (c == '(' || c == '[')
+// 			st.push({c, -1});
+// 		// 닫는 괄호일 경우
+// 		else{
+// 			// 숫자가 아니면
+// 			char t = st.top().first;
+// 			if (t != '#'){
+// 				if (t == '['){
+// 					st.pop();
+// 					st.push({'#', 3});
+// 				}
+// 				else if (t == '('){
+// 					st.pop();
+// 					st.push({'#', 2});
+// 				}
+// 			}
+// 			// first가 숫자면
+// 			else{
+// 				if (st.top().first == '#'){
+// 					int tmp = 0;
+// 					while(st.top().first == c){
+// 						tmp += st.top().second * 3;
+// 						st.pop();
+// 					}
+// 					st.pop();
+// 					cout << tmp << "\n";
+// 				}
+// 			}
+// 		}
+// 	}
+// }
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	string str;
-	int res = 0;
-	stack<char> st;
-	cin >> str;
-
-	for(int i = 0 ; i < str.length() ; i++){
-		if (str[i] == '(') st.push(str[i]);
-		else{
-			if (str[i - 1] == '('){ // 레이저일 경우
-				st.pop();
-				res += st.size();
-			}
-			else{
-				st.pop();
-				res++;
-			}
- 		}
-	}
-	cout << res << "\n";
+int main(void)
+{
+	cout << "hello world!";
 }
